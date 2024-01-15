8faa71067fd01ce87937d32b388ebc2f67def5b1
Somie1 <somelezesipayile@gmail.com>
2024-01-15 10:20:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //the test that can be either true or false
+            bool isMale = true;
+            if (isMale)
+            {
+                Console.WriteLine("You are male");
+            }
+            else
+            {
+                Console.WriteLine("You are female");
+            }
+            //you can also test 2 conditions with and ie && in C#, or is ||
+            bool isTall = false;
+            if (isTall && isMale)
+            {
+                Console.WriteLine("You are both male and tall");
+            }
+            else if (!isTall && isMale)
+            {
+                Console.WriteLine("you are not tall but you are male");
+            }
+            else
+            {
+                Console.WriteLine("You are either not tall or not a male");
+            }
+            Console.ReadLine();
+        }
+    }
+}
